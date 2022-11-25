a63f91582e981eadee9db5cbbef46c5c8da64895
victorFloresDubon <vdubon@outlook.com>
2022-11-24 22:40:53 -0600

    Creado el controlador FacturacionController

---
+++ src/main/java/umg/bd1/proyectofinal/controlador/FacturacionController.java
@@ -0,0 +1,14 @@
+package umg.bd1.proyectofinal.controlador;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FacturacionController {
+
+    @GetMapping("facturacion/factura")
+    public String getFormularioFactura(Model modelo){
+        return "Facturacion/crear-facturacion";
+    }
+}
